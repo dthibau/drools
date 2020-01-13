3d1a1f1cc2b78abcb94a11beae38f6ae74302bec
David THIBAU <david.thibau@gmail.com>
2020-01-13 09:24:08 +0100

    Initial commit

---
+++ TPS/Optional_DroolsFlow/src/main/java/org/formation/drools/test/TestFire.java
@@ -0,0 +1,55 @@
+package org.formation.drools.test;
+
+import org.formation.drools.helper.RuleRunner;
+import org.formation.drools.model.stateful.Fire;
+import org.formation.drools.model.stateful.Room;
+import org.formation.drools.model.stateful.Sprinkler;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.kie.api.runtime.rule.FactHandle;
+
+public class TestFire {
+	static RuleRunner ruleRunner;
+
+	Room kitchen = new Room("kitchen");
+	Room salon = new Room("salon");
+	Room chambre = new Room("chambre");
+	Sprinkler sprinklerKitchen = new Sprinkler(kitchen);
+	Sprinkler sprinklerSalon = new Sprinkler(salon);
+	Sprinkler sprinklerChambre = new Sprinkler(chambre);
+	
+	@BeforeClass
+	public static void init(){
+		ruleRunner = new RuleRunner();
+	}
+
+	@Test
+	public void testProcess(){
+        Object[] facts = new Object[] {kitchen,
+        		salon,
+        		chambre,
+        		sprinklerKitchen,
+        		sprinklerSalon, 
+        		sprinklerChambre, 
+        		new Fire(kitchen),
+        		new Fire(salon)};
+
+        FactHandle[] factHandles = ruleRunner.setStatefulRules(facts);
+        ruleRunner.startProcess("Fire");
+        ruleRunner.fireAllRules();
+        System.out.println("-- On enlève le feu dans la cuisine "+factHandles[6]);
+        ruleRunner.retractFact(factHandles[6]);
+        ruleRunner.signalEvent("DetectChange");
+        ruleRunner.fireAllRules();
+        
+        System.out.println("-- On enlève le feu dans le salon "+factHandles[7]);
+        ruleRunner.retractFact(factHandles[7]);
+        ruleRunner.signalEvent("DetectChange");
+        ruleRunner.fireAllRules();
+         System.out.println("-- On arrête le système");
+        ruleRunner.signalEvent("SHUTDOWN");
+	}
+
+
+
+}
